41c391b8d95c18188f79770d267b3863eacbc800
Holger Mueller <9jholger@gmail.com>
2020-03-31 12:26:34 -0500

    refactored profile page

--- src/components/Pages/Profile/EditUserInfo.vue
+++ src/components/Pages/Profile/EditUserInfo.vue
@@ -1,27 +1,41 @@
 <template>
   <div id="editUserInfo">
-    <v-card class="user-info">
-      <v-card-title> Hello, {{ userInfoToDisplay.displayName }} </v-card-title>
+    <v-dialog v-model="dialog" width="500">
+      <template v-slot:activator="{ on }">
+        <v-btn v-on="on" block>
+          Edit Profile
+        </v-btn>
+      </template>
 
-      <v-card-text>
-        <v-form class="form">
-          <v-text-field
-            :value="userInfoToDisplay.displayName"
-            outlined
-          ></v-text-field>
-          <v-text-field
-            :value="userInfoToDisplay.email"
-            outlined
-          ></v-text-field>
-        </v-form>
-      </v-card-text>
+      <v-card class="user-info">
+        <v-card-title> Edit Profile:</v-card-title>
 
-      <v-divider></v-divider>
+        <v-divider></v-divider>
 
-      <v-card-actions>
-        <v-btn :loading="loading" :disabled="loading" block>SUBMIT</v-btn>
-      </v-card-actions>
-    </v-card>
+        <v-card-text>
+          <v-form class="form">
+            <v-text-field
+              label="Username"
+              :value="displayName"
+              outlined
+            ></v-text-field>
+            <v-text-field label="email" :value="email" outlined></v-text-field>
+
+            <v-textarea outlined label="Bio"></v-textarea>
+          </v-form>
+        </v-card-text>
+
+        <v-divider></v-divider>
+
+        <v-card-actions>
+          <v-btn @click="dialog = false">Cancel</v-btn>
+
+          <v-spacer></v-spacer>
+
+          <v-btn :loading="loading" :disabled="loading">SUBMIT</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
   </div>
 </template>
 
@@ -29,13 +43,27 @@
 export default {
   name: "UpdateUserInfo",
 
+  props: {
+    displayName: {
+      type: String,
+      required: true
+    },
+
+    email: {
+      type: String,
+      required: true
+    }
+  },
+
+  data() {
+    return {
+      dialog: false
+    };
+  },
+
   components: {},
 
   computed: {
-    userInfoToDisplay() {
-      return this.$store.getters.user;
-    },
-
     loading() {
       return this.$store.getters.loading;
     }
--- src/components/Pages/Profile/Profile.vue
+++ src/components/Pages/Profile/Profile.vue
@@ -1,10 +1,25 @@
 <template>
   <div id="profile">
     <v-card class="profile-actions">
-      <EditUserInfo />
+      <v-card-title> Hello, {{ userInfoToDisplay.displayName }}</v-card-title>
 
-      <DeleteAccnt />
+      <v-card-text>
+        {{ userInfoToDisplay.displayName }}
+
+        {{ userInfoToDisplay.email }}
+      </v-card-text>
+
+      <v-card-actions>
+        <EditUserInfo
+          :displayName="userInfoToDisplay.displayName"
+          :email="userInfoToDisplay.email"
+        />
+
+        <DeleteAccnt />
+      </v-card-actions>
     </v-card>
+
+    <v-card>List of user posts will appear here.</v-card>
   </div>
 </template>
 
@@ -18,6 +33,12 @@ export default {
   components: {
     DeleteAccnt,
     EditUserInfo
+  },
+
+  computed: {
+    userInfoToDisplay() {
+      return this.$store.getters.user;
+    }
   }
 };
 </script>
