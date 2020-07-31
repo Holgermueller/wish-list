08f380df745b66d6b500e433e3a36bdf58e98447
Holger Mueller <9jholger@gmail.com>
2020-07-31 11:16:26 -0500

    removed config file from repo to hide keys

---
+++ src/firebase/configExample.js
@@ -0,0 +1,10 @@
+export default {
+  apiKey: "xxxxxx",
+  authDomain: "xxxxxx",
+  databaseURL: "xxxxxx",
+  projectId: "xxxxxx",
+  storageBucket: "xxxxxx",
+  messagingSenderId: "xxxxxx",
+  appId: "xxxxxx",
+  measurementId: "xxxxxx",
+};
